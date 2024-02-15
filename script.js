53d545fefe4abeee9b1a456962ad0db077e6c396
wstypr <wsetyapermadi@gmail.com>
2024-02-15 15:35:42 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const rules = {
+  rock: "scissors",
+  paper: "rock",
+  scissors: "paper",
+};
+
+function getComputerChoice() {
+  // randomly return rock/paper/scissors
+}
+
+function getPlayerSelection() {
+  // prompt player for rock/paper/scissors
+}
+
+function playRound(playerSelection, computerSelection) {
+  // play 1 round
+}
+
+function playGame() {
+  // play 5 rounds of game
+}
